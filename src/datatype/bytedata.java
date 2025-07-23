51bef8efc3365c1c853bf2661c70a253aed93ae0
Rehan Ansari <rehan831@gmail.com>
2025-07-23 12:46:20 +0530

    core java

---
+++ src/datatype/bytedata.java
@@ -0,0 +1,24 @@
+package datatype;
+
+public class bytedata {
+public static void main(String[]args) {
+	byte b = 127;
+	byte a = -128;
+	System.out.println("1 byte 8 bits");
+	System.out.println(b +" " +a);
+	short s = 32767;
+	short d = -32766;
+	System.out.println("2 byte 16 bits");
+	System.out.println(s +" " +d);
+	int i = 2147483647;
+	int l = -2147483648;
+	System.out.println("4 byte 32 bits");
+	System.out.println(i +" " +l);
+	long L = 34253455l;
+	long E = -5466466l;
+	System.out.println("8 byte 64 bits");
+	System.out.println(L +" " +E);
+	
+}
+  
+}

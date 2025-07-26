8b6f67ded29ffdd2165053ab23edd834c679fcc7
Rehan Ansari <rehan831@gmail.com>
2025-07-26 23:13:06 +0530

    core java

---
+++ src/datatype/LiteralsJava.java
@@ -0,0 +1,74 @@
+package datatype;
+
+public class LiteralsJava {
+	public static void main(String[]args) {
+		int a = 2147483647;
+		int c = 0xface;
+		int b = 0777;
+		int d = 0b1010;
+		
+		//this is for byte byte range and literals
+		byte be = 127;
+		byte re = -128;
+		byte reh = 0x77;
+		byte ans = 077;
+		byte ansari = 0b1010;
+		
+		//this is for short range and 
+		short s = 32767;
+		short e = -32768;
+		short r = 0xfac;
+		short h = 0777;
+		short t = 0b1011;
+		 
+		//this is for long 
+		long l = 100000000l;
+		long fd = 0xface;
+		long hg = 0777;
+		long tr = 0b1010;
+		 // this is for floating point literlas float 
+		
+		float flo = 1.8956f;
+		float fle = 1.3e4f;
+		float file = 0xface;
+		float fil = 0777;
+		float rte = 0b10101;
+		
+		//this is for double literals 
+		
+		double rehan = 123.546;
+		double is = 0777;
+		double et = 0xface;
+		double ty = 0x123;
+		double twb = 12.12;
+		int bhai = 0x1546;
+		
+		
+		//this is for boolean 
+		
+		boolean st = true;
+		boolean ts = false;
+		 
+		// this is for char
+		
+		char ch = 'a';
+		char hc =65;
+		char nb = '\u0001';
+		char lg = '\n';
+		
+		//this is for null type lkiterals
+		
+		String  uy = null;
+		System.out.println(uy);
+		System.out.println(hc);
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
